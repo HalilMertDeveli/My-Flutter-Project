16757b7653c09dce60da6336fcc6e716006d3e81
ted <alone_coder@gmial.com>
2023-01-26 05:53:48 +0300

    we created empt statfull class for desing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(MyApp());
+  runApp(ForAuthDesing());
 }
 
 class MyApp extends StatelessWidget {
@@ -32,6 +32,69 @@ class MyApp extends StatelessWidget {
           onPageBuilder: (context, AsyncSnapshot<AppUser?> snapshot) {
             return PageSelector(snapshot: snapshot);
           },
+        ),
+      ),
+    );
+  }
+}
+
+/*
+  Takip ettiğin Youtube uygulamasında ki tasarımı al , yazdıgın firebase ile birleştir
+  aldıgın bu değerleri metotlara ilet ve giriş sistemini tamamla 
+
+  algoritma:
+    1-)Youtube 'daki giriş sistemini buraya entegre et 
+    2-) veriler form kullanarak al ve bu iki yapıyı biribiri ile bağla 
+      2-a) Bunu yaparken git kullan ve bildmediğin yerde googla ve o problemi nasıl çözecegini araştır
+           problemi çözdikkten sonra diğer probleme ilerle ,adım-adım 
+
+
+
+ */
+
+class ForAuthDesing extends StatefulWidget {
+  ForAuthDesing({super.key});
+
+  @override
+  State<ForAuthDesing> createState() => _ForAuthDesingState();
+}
+
+class _ForAuthDesingState extends State<ForAuthDesing> {
+  final formKey = GlobalKey<FormState>();
+  String? comingValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Auth Desing'),
+        ),
+        body: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                onSaved: (newValue) {
+                  setState(() {
+                    comingValue = newValue;
+                  });
+                },
+              ),
+              TextFormField(),
+              MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    // formKey.currentState?.save();
+                    print(comingValue);
+                  });
+                },
+                child: Icon(Icons.save),
+              )
+            ],
+          ),
         ),
       ),
     );
